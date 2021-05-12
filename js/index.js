931b77cba28b382df2b9fc2f5054ba652e8456c5
Beautifullagency <Javiercossio@beautifullagency.com>
2021-05-12 15:29:24 -0300

    Subida de proyecto

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const t = document.getElementsByClassName('t')
+console.log(t)
+for (let item of t) {
+  console.log(item);
+  item.addEventListener('mouseover', function changeDefOver(e) {
+    console.log(item.getElementsByClassName('t-title')[0])
+    item.getElementsByClassName('t-title1')[0].classList.add('hidde')
+    item.getElementsByClassName('m')[0].classList.remove('contract')
+    item.getElementsByClassName('m')[0].classList.add('expand')
+  }
+  );
+  item.addEventListener('mouseout', function changeDefOut(e) {
+    item.getElementsByClassName('t-title1')[0].classList.remove('hidde')
+    item.getElementsByClassName('m')[0].classList.add('contract')
+    item.getElementsByClassName('m')[0].classList.remove('expand')    
+    
+
+  });
+
+
+
+}
+
+
+grecaptcha.ready(function() {
+  grecaptcha.execute( '6LfFf8oaAAAAAJEnFqr1kZAeobLezLOYXUrGS5J9', {action: 'homepage'}).then(function(token) {
+      // Add your logic to submit to your backend server here.
+      $('#google-response').val(token)
+  });
+});
+
